18064409070fcffd572cbb49253214945e3b7cea
Julio Miguel Kuchuminski <juliomckuchuminski@gmail.com>
2026-06-11 20:04:12 -0300

    frontend - feature: Base do dashboard criada

--- RA3 POO/demo/src/main/java/module-info.java
+++ RA3 POO/demo/src/main/java/module-info.java
@@ -1,8 +1,12 @@
-module com.example.demo {
+module com.seu.projeto { // O nome do módulo deve bater com o do seu pom.xml ou package
+    // Dependências necessárias
     requires javafx.controls;
     requires javafx.fxml;
 
+    // Permite que o JavaFX use reflexão para instanciar suas classes
+    // O 'to javafx.fxml' limita esse acesso apenas ao módulo do JavaFX, aumentando a segurança
+    opens com.capitulozero.view to javafx.fxml;
 
-    opens com.example.demo to javafx.fxml;
-    exports com.example.demo;
+    // (Opcional) Exporta se outras classes precisarem acessar publicamente
+    exports com.capitulozero.view;
 }
