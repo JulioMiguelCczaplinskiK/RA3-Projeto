8828c2e29cd0ef8289609608df8331131e229b5a
Julio Miguel Kuchuminski <juliomckuchuminski@gmail.com>
2026-06-16 20:43:38 -0300

    frontend - feature: Botoes agora mostram o conteudo das abas

---
+++ RA3 POO/demo/src/main/java/com/capitulozero/view/AutoresView.java
@@ -0,0 +1,26 @@
+package com.capitulozero.view;
+
+import com.capitulozero.config.Theme;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+public class AutoresView extends VBox {
+    public AutoresView(double largura, double altura) {
+        this.setPrefSize(largura, altura);
+        this.setMaxSize(largura, altura);
+        this.setPadding(new Insets(20));
+        this.setBackground(new Background(new BackgroundFill(Theme.COR_BACKGROUND_TERCEARIO, CornerRadii.EMPTY, Insets.EMPTY)));
+        this.setAlignment(Pos.CENTER);
+
+        Label label = new Label("👤 Painel de Autores");
+        label.setTextFill(Theme.COR_TEXT_PRIMARIO);
+        label.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
+        this.getChildren().add(label);
+    }
+}
